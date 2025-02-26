f33ef32c687e0f5dff32682a27b0b147f8748c01
MUHAMMAD MUBEEN <110968985+MubeenAnjum@users.noreply.github.com>
2025-02-26 14:50:58 +0400

    Create init_database.sql
    Creating the database and schemas for our datawarehouse project

---
+++ scripts/init_database.sql
@@ -0,0 +1,49 @@
+-- Check if database exists, if not, create it
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    CREATE DATABASE DataWarehouse;
+    PRINT 'Database DataWarehouse created successfully';
+END
+ELSE
+BEGIN
+    PRINT 'Database DataWarehouse already exists';
+END
+GO
+
+-- Use the database
+USE DataWarehouse;
+GO
+
+-- Create schemas with checks
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'bronze')
+BEGIN
+    EXEC('CREATE SCHEMA bronze');
+    PRINT 'Schema bronze created successfully';
+END
+ELSE
+BEGIN
+    PRINT 'Schema bronze already exists';
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'silver')
+BEGIN
+    EXEC('CREATE SCHEMA silver');
+    PRINT 'Schema silver created successfully';
+END
+ELSE
+BEGIN
+    PRINT 'Schema silver already exists';
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'gold')
+BEGIN
+    EXEC('CREATE SCHEMA gold');
+    PRINT 'Schema gold created successfully';
+END
+ELSE
+BEGIN
+    PRINT 'Schema gold already exists';
+END
+GO
